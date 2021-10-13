32f23ae8f341d49c8c10a2d9eedd37d3ff8e7e3a
Maíra Bassetto <mairabassetto@gmail.com>
2021-10-13 12:34:20 -0300

    Getting use profile by ID and show profile of a non existing user

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -2,15 +2,20 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ShowUserProfileUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): void {
+        const userAlredyExists = this.usersRepository.findById(user_id);
+        if (userAlredyExists) {
+            throw new Error("User Alredy Exists!");
+        }
+
+        this.usersRepository.findById(user_id);
+    }
 }
 
 export { ShowUserProfileUseCase };
